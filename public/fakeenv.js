8d7e8821416e718ee3f184576d50bb2923f20167
Vcaze <viktor.nordenberg1@gmail.com>
2026-03-18 23:44:01 +0100

    Changed API domain to the onrender one

--- public/fakeenv.js
+++ public/fakeenv.js
@@ -2,7 +2,8 @@
 // These replace the .env file when running locally in browser
 
 const env = {
-  VITE_PUBLIC_API_URL: "http://localhost:1337/api"
+//   VITE_PUBLIC_API_URL: "http://localhost:1337/api"
+  VITE_PUBLIC_API_URL: "https://wc26-api.onrender.com/api"
 };
 
 // Export for use in the application
